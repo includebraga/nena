5e4089d1665187b06062c97f3427abe23309ebd1
Queiroga <queiroga.fernandes@hotmail.com>
2017-02-10 21:22:50 +0000

    Added has_may payments in Project and User (#5)

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ApplicationRecord
   belongs_to :organization
   has_many :user_project_payments
   has_many :users, through: :user_project_payments
+  has_many :payments, through: :user_project_payments
 end
--- db/migrate/20170210205636_create_user_project_payments.rb
+++ db/migrate/20170210205636_create_user_project_payments.rb
@@ -3,7 +3,8 @@ class CreateUserProjectPayments < ActiveRecord::Migration[5.0]
     create_table :user_project_payments do |t|
       t.references :projects
       t.references :users
-
+      t.references :payments
+      
       t.timestamps
     end
   end
