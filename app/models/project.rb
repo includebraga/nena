4d57a0e4483131f4618d609ce7c50fd2c16e705d
QueirogaAmorim <queiroga.fernandes@hotmail.com>
2017-02-10 21:05:37 +0000

    fixed error

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
   belongs_to :organization
-  has_many :user_organization_payments
+  has_many :user_project_payments
   has_many :users, through: :user_project_payments
 end
