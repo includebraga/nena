86a4aab3cb43f1b464708ed8a8ca7f1879412b92
joseazevedo88 <josemendesazevedo@gmail.com>
2017-02-10 23:51:48 +0000

    Code review

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,15 @@ class User < ApplicationRecord
   validates :email, :name, presence: true
 
   def self.create_with_omniauth(auth)
-  
-  user = find_or_create_by(uid: auth['uid'], provider:  auth['provider'])
-  user.email = "#{auth['uid']}@#{auth['provider']}.com"
-  user.name = auth['info']['name']
-  if User.exists?(user)
-    user
-  else
-    user.save!
-    user
+    user = find_or_create_by(uid: auth['uid'], provider:  auth['provider'])
+    user.email = "#{auth['uid']}@#{auth['provider']}.com"
+    user.name = auth['info']['name']
+    
+    if User.exists?(user)
+      user
+    else
+      user.save!
+      user
+    end
   end
-end
 end
