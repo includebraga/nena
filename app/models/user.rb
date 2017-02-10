82f75b38aa7f81e5cc7c42e3d0a6828ed68cd328
QueirogaAmorim <queiroga.fernandes@hotmail.com>
2017-02-10 21:16:43 +0000

    Added has_may payments in Project and User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   has_many :organizations, through: :organization_users
   has_many :user_project_payments
   has_many :projects, through: :user_project_payments
+  has_many :payments, through: :user_project_payments
   validates :email, :name, presence: true
 end
