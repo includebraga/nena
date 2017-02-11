840c1122ca5a13883fa643ad9957ff236d3d8e73
Fernando Mendes <devfrmendes@gmail.com>
2017-02-11 16:54:25 +0000

    HOTFIX: Revert schema errors

--- db/migrate/20170211163838_rename_payments_id.rb
+++
@@ -1,5 +0,0 @@
-class RenamePaymentsId < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :user_project_payments, :payments_id, :payment_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211163838) do
+ActiveRecord::Schema.define(version: 20170211154200) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "city"
