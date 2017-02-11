d997b7f4490ab3d7002f53f24dd337f9fe63fe3f
Fernando Mendes <devfrmendes@gmail.com>
2017-02-11 13:20:53 +0000

    Adds new initiative button

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -4,4 +4,9 @@ class Organization < ApplicationRecord
   has_many :projects
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+
+  def admin?(user)
+    OrganizationUser.where(user_id: user.id, organization_id: id).try(:admin)
+  end
 end
