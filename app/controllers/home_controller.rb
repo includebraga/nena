4d5c00e6b5eace3d4dfba6101744f964ece9e002
JoaoFCosta <dinojoaocosta@gmail.com>
2017-02-11 16:57:38 +0000

    Add dynamic user statistics.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     if current_user
       @projects = Project.where(location: current_user.location)
+      @user     = current_user
     else
       render 'unauthenticated'
     end
