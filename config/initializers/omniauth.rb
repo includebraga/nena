7f90a27ebf5e0c962fab656f59750a0e5074b725
joseazevedo88 <josemendesazevedo@gmail.com>
2017-02-11 04:49:59 +0000

    Changed landing page text

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.secrets.facebook_app_id,
-  Rails.application.secrets.facebook_app_secret, scope: 'public_profile', info_fields: 'id,name,link'
+  provider :facebook, Rails.application.secrets.facebook_app_id, Rails.application.secrets.facebook_app_secret, scope: 'public_profile,email,user_location,user_photos', image_size: 'large'
 end
